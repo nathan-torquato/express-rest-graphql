27a5640b56134f55f0b41e985746bd33cc9bbb79
Mauricio <mauricionatacci98@gmail.com>
2023-03-27 13:52:44 -0300

    feat: add typegraphql

--- src/graphql.server.ts
+++ src/graphql.server.ts
@@ -3,13 +3,22 @@ import 'reflect-metadata'
 import { ApolloServer, gql } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { resolvers } from './resolvers'
+import { readFileSync } from 'fs'
+
+const typeDefs = readFileSync('/schema.gql', { encoding: 'utf-8' })
 
 export const buildGraphQLServer = async (app: Express) => {
 	const schema = await buildSchema({
 		resolvers,
+		emitSchemaFile: {
+			path: __dirname + '/schema.gql',
+			commentDescriptions: true,
+			sortedSchema: false,
+		},
 	})
 
 	const server = new ApolloServer({
+		typeDefs,
 		schema,
 	})
 
